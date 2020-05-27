691b9e466bb1429bf67b5524e418167461ee8227
kuuki219 <65888354+kuuki219@users.noreply.github.com>
2020-05-27 21:45:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,83 +4,20 @@ import os
 import traceback
 import asyncio
 
-bot = commands.Bot(command_prefix='/')
-client = commands.Bot(command_prefix='.')
-token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()  
+token = os.environ['DISCORD_BOT_TOKEN']  
 
+ID_CHANNEL_README = 715173934378778697 # 該当のチャンネルのID  
+ID_ROLE_WELCOME = 715183433906782316 # 付けたい役職のID  
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event  
+async def on_raw_reaction_add(payload):  
+    channel = client.get_channel(payload.channel_id)  
+    if channel.id == ID_CHANNEL_README:  
+        guild = client.get_guild(payload.guild_id)  
+        member = guild.get_member(payload.user_id)  
+        role = guild.get_role(ID_ROLE_WELCOME)  
+        await member.add_roles(role)  
+        await channel.send('いらっしゃいませ！')  
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-bot.run(token)
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-@client.command()
-async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
-    cnt, settime = int(cnt), float(settime)
-    reaction_member = [">>>"]
-    test = discord.Embed(title=about,colour=0x1e90ff)
-    test.add_field(name=f"あと{cnt}人 募集中\n", value=None, inline=True)
-    msg = await ctx.send(embed=test)
-    #投票の欄
-    await msg.add_reaction('⏫')
-    await msg.add_reaction('✖')
-
-    def check(reaction, user):
-        emoji = str(reaction.emoji)
-        if user.bot == True:    # botは無視
-            pass
-        else:
-            return emoji == '⏫' or emoji == '✖'
-
-    while len(reaction_member)-1 <= cnt:
-        try:
-            reaction, user = await client.wait_for('reaction_add', timeout=settime, check=check)
-        except asyncio.TimeoutError:
-            await ctx.send('残念、人が足りなかったようだ...')
-            break
-        else:
-            print(str(reaction.emoji))
-            if str(reaction.emoji) == '⏫':
-                reaction_member.append(user.name)
-                cnt -= 1
-                test = discord.Embed(title=about,colour=0x1e90ff)
-                test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
-                await msg.edit(embed=test)
-
-                if cnt == 0:
-                    test = discord.Embed(title=about,colour=0x1e90ff)
-                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
-                    await msg.edit(embed=test)
-                    finish = discord.Embed(title=about,colour=0x1e90ff)
-                    finish.add_field(name="おっと、メンバーがきまったようだ",value='\n'.join(reaction_member), inline=True)
-                    await ctx.send(embed=finish)
-
-            elif str(reaction.emoji) == '✖':
-                if user.name in reaction_member:
-                    reaction_member.remove(user.name)
-                    cnt += 1
-                    test = discord.Embed(title=about,colour=0x1e90ff)
-                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
-                    await msg.edit(embed=test)
-                else:
-                    pass
-        # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
-        await msg.remove_reaction(str(reaction.emoji), user)
-
-
-client.run(token)
+client.run(token)  
